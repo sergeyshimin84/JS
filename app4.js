8cac3c93bc6f5e753e20d2c13ca1145041898dff
Sergey <sergey.shimin84@gmail.com>
2021-10-02 23:34:17 +0700

    homework4.3

--- app4.js
+++ app4.js
@@ -10,52 +10,52 @@ Product.prototype.make25PercentDiscount = function () {
     alert(this.price * 0.75);
 }
 
-let obj1 = new Product('Sergey', 50);
+let obj1 = new Product(Product(50[1]));
 obj1.make25PercentDiscount();
 
-//ES6 
-class Product {
-    constructor(name, price) {
-        this.name = name;
-        this.price = price;
-    }
+// //ES6 
+// class Product {
+//     constructor(name, price) {
+//         this.name = name;
+//         this.price = price;
+//     }
 
-    make25PercentDiscount() {
-        alert(this.price * 0.75);
-    }
-}
+//     make25PercentDiscount() {
+//         alert(this.price * 0.75);
+//     }
+// }
 
-let obj2 = new Product('Sergey', 50);
-obj2.make25PercentDiscount();
+// let obj2 = new Product('Sergey', 50);
+// obj2.make25PercentDiscount();
 
-// Задание 1.2
-function Post(author, text, date) {
-    this.author = author;
-    this.text = text;
-    this.date = date;
-}
+// // Задание 1.2
+// function Post(author, text, date) {
+//     this.author = author;
+//     this.text = text;
+//     this.date = date;
+// }
 
-Post.prototype.edit = function () {
-    alert(this.text);
-};
+// Post.prototype.edit = function () {
+//     alert(this.text);
+// };
 
-let bObj = new Post('Author', 'text', 'date');
-bObj.edit();
+// let bObj = new Post('Author', 'text', 'date');
+// bObj.edit();
 
-//ES6 
-class Post {
-    constructor(author, text, date) {
-        this.author = author;
-        this.text = text;
-        this.date = date;
-    }
+// //ES6 
+// class Post {
+//     constructor(author, text, date) {
+//         this.author = author;
+//         this.text = text;
+//         this.date = date;
+//     }
 
-    edit() {
-        alert(this.text);
-    }
-}
+//     edit() {
+//         alert(this.text);
+//     }
+// }
 
-let bObj = new Post('Author', 'text', 'date');
-bObj.edit();
+// let bObj = new Post('Author', 'text', 'date');
+// bObj.edit();
 
 
