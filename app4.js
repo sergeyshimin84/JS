cc8a403eaa0acb4298c04349c41be2dc7ee6aad5
Sergey <sergey.shimin84@gmail.com>
2021-10-03 18:45:05 +0700

    homework5

--- app4.js
+++ app4.js
@@ -1,4 +1,20 @@
 'use strict';
+// Задание 1
+function getNumber(num) {
+    if (!Number.isInteger(num) || num < 0 || num > 999) {
+        console.log('Число должно быть целым от 0 до 999.');
+        return {};
+    }
+
+    return {
+        hundreds: Math.floor(num / 100),
+        tens: Math.floor(num / 10) % 10,
+        units: num % 10,
+    }
+}
+
+console.log(getNumber(456));
+
 // Задание 1.1
 // ES5
 function Product(name, price) {
@@ -7,55 +23,100 @@ function Product(name, price) {
 }
 
 Product.prototype.make25PercentDiscount = function () {
-    alert(this.price * 0.75);
+    this.price = this.price - (this.price * 25 / 100);
+};
+
+const product1 = new Product('Товар', 50);
+product1.make25PercentDiscount();
+
+console.log(product1);
+
+//ES6 
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    make25PercentDiscount() {
+        this.price = this.price - (this.price * 25 / 100);
+    }
 }
 
-let obj1 = new Product(Product(50[1]));
-obj1.make25PercentDiscount();
+const product1 = new Product('Товар', 50);
+product1.make25PercentDiscount();
 
-// //ES6 
-// class Product {
-//     constructor(name, price) {
-//         this.name = name;
-//         this.price = price;
-//     }
+console.log(product1);
 
-//     make25PercentDiscount() {
-//         alert(this.price * 0.75);
-//     }
-// }
+// Задание 1.2
+//ES5
+function Post(author, text, date) {
+    this.author = author;
+    this.text = text;
+    this.date = date;
+}
 
-// let obj2 = new Product('Sergey', 50);
-// obj2.make25PercentDiscount();
+Post.prototype.edit = function (text) {
+    this.text = text;
+}
 
-// // Задание 1.2
-// function Post(author, text, date) {
-//     this.author = author;
-//     this.text = text;
-//     this.date = date;
-// }
+const post1 = new Post('Serg', 'text', new Date());
+console.log(post1);
+post1.edit('new text');
+console.log(post1);
 
-// Post.prototype.edit = function () {
-//     alert(this.text);
-// };
 
-// let bObj = new Post('Author', 'text', 'date');
-// bObj.edit();
+function AttachedPost(author, text, date) {
+    Post.call(this, author, text, date);
+    this.highlighted = false;
+}
 
-// //ES6 
-// class Post {
-//     constructor(author, text, date) {
-//         this.author = author;
-//         this.text = text;
-//         this.date = date;
-//     }
+AttachedPost.prototype = Object.create(Post.prototype);
+AttachedPost.prototype.constructor = AttachedPost;
 
-//     edit() {
-//         alert(this.text);
-//     }
-// }
+AttachedPost.prototype.makeTextHighlighted = function () {
+    this.highlighted = true;
+}
 
-// let bObj = new Post('Author', 'text', 'date');
-// bObj.edit();
+const attached1 = new AttachedPost('admin', 'text2', new Date());
+console.log(attached1);
+attached1.makeTextHighlighted();
+attached1.edit('new text2');
+console.log(attached1);
 
+//ES6 
+function Post {
+    constructor(author, text, date) {
+        this.author = author;
+        this.text = text;
+        this.date = date;
+    }
+
+    edit() {
+        this.text = text;
+    }
+}
+
+const post1 = new Post('Serg', 'text', new Date());
+console.log(post1);
+post1.edit('new text');
+console.log(post1);
+
+
+class AttachedPost extends Post {
+    constructor(author, text, date) {
+        super(author, text, date);
+        this.highlighted = false;
+    }
+
+    makeTextHighlighted() {
+        this.highlighted = true;
+    }
+}
+
+const attached1 = new AttachedPost('admin', 'text2', new Date());
+console.log(attached1);
+attached1.makeTextHighlighted();
+attached1.edit('text2');
+console.log(attached1);
 
